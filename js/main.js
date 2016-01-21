1f3ddd57a210e55ffe5a2744046d45620ce40f67
Varga Zoltán <waress76@gmail.com>
2016-01-21 20:28:26 +0100

    végső állapot

--- js/main.js
+++ js/main.js
@@ -8,3 +8,27 @@ function _q( selector ) { // aláhúzás miatt rövid és gyorsan elérhető
     
     return elements;
 }
+
+// Profil beviteli mező megjelenítése
+function showProfile() {
+    /*
+    // megkeressük a profile div-et
+    var div = document.querySelector(".profile-div");
+    
+    // írassuk ki, h megtaláltuk-e
+    // console.log(div);
+    
+    // új osztály név hozzáadása
+    div.classList.add("show");
+    */
+    
+    // vizsgálat, hogy megkapta-e a show-t, ha igen akkor el kell venni
+    var div = document.querySelector(".profile-div");
+    
+    // ha a show hozzá van adva: elrejtjük, különben megjelenítjük
+    if (div.classList.contains("show")) {
+        div.classList.remove("show");
+    } else {
+        div.classList.add("show");
+    }
+}
